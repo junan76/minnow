b0563cd80760010fa3484f849e191a361a3b8cc3
junan76 <junan76@163.com>
2023-11-20 10:56:43 +0800

    [check2]: done with wrap32.

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -5,15 +5,20 @@ using namespace std;
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return Wrap32 { static_cast<uint32_t>( n & 0xFFFFFFFF ) + zero_point.raw_value_ };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint32_t checkpoint_seqno = ( checkpoint & 0xFFFFFFFF ) + zero_point.raw_value_;
+  uint32_t d_left
+    = checkpoint_seqno >= raw_value_ ? checkpoint_seqno - raw_value_ : 0x100000000 - raw_value_ + checkpoint_seqno;
+  uint32_t d_right = 0x100000000 - d_left;
+
+  if ( d_left < d_right ) {
+    return checkpoint + ( checkpoint < d_left ? 0x100000000 : 0 ) - d_left;
+  } else {
+    return checkpoint + d_right;
+  }
 }
